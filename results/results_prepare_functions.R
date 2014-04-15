fd254f4ffc3e07bab225158f9aa0250bf0e5611c
wellermatt <wellermatt@hotmail.com>
2014-04-15 13:38:40 +0100

    Results analysis for single item

--- results/results_prepare_functions.R
+++ results/results_prepare_functions.R
@@ -84,35 +84,3 @@ results = f_consolidate.errors(upc=NULL)
 ## load the consolidated results
 #results = readRDS("E:/data/errors/all.rds")
 
-
-
-res.counts = results[,.N,by=list(method,freq,freq.cycle, Level)]
-dcast(res.counts, freq+freq.cycle+Level~method, fun.aggregate=sum,value.var="N")
-
-
-dcast(results, Level+fc.item~freq+freq.cycle+method, fun.aggregate= median,na.rm=TRUE, value.var="ape")
-dcast(results, Level+freq+freq.cycle~method, fun.aggregate= median,na.rm=TRUE, value.var="ape")
-
-res.summary = f_results.summarise(results)
-
-
-library(ggplot2)
-ggplot(data = res.summary[lvl==1], aes(x = method, y = mdape)) + geom_point() +  coord_flip() + facet_wrap(~freq)+
-    ylim(0,0.5)
-
-
-ggplot(data=results[lvl==1], aes(x= fc.item, y=ase.naive, colour = method)) + geom_boxplot() + facet_wrap(~freq) +coord_flip()
-ggplot(data=results[lvl==1], aes(x= fc.item, y=rae.naive)) + geom_boxplot() + coord_flip()
-
-
-#res=f_results.load("E:/data/errors/reg_52_52_beer_3_.rds")
-#res.summary = f_results.summarise(res)
-
-
-
-x = readRDS("E:/data/errors/ets_52_12_3_beer_00-01-18200-53030.rds")
-max(x$o) ;x
-
-x[,max(o),by=fc.item]
-tail(x,100)
-
--- results/results_single_item.R
+++ results/results_single_item.R
@@ -4,10 +4,47 @@ library(reshape2)
 setwd(pth.dropbox.code) ; source("./results/results_prepare_functions.R")
 
 results = f_consolidate.errors()
+res.counts = results[,.N,by=list(method,freq,freq.cycle, Level)]
+dcast(res.counts, freq+freq.cycle+Level~method, fun.aggregate=sum,value.var="N")
+
+
+dcast(results, Level+fc.item~freq+freq.cycle+method, fun.aggregate= median,na.rm=TRUE, value.var="ape")
+dcast(results, Level+freq+freq.cycle~method, fun.aggregate= median,na.rm=TRUE, value.var="ape")
+
+res.summary = f_results.summarise(results)
+
+
+library(ggplot2)
+ggplot(data = res.summary[lvl==1], aes(x = method, y = mdape)) + geom_point() +  coord_flip() + facet_wrap(~freq)+
+    ylim(0,0.5)
+
+
+ggplot(data=results[lvl==1], aes(x= fc.item, y=ase.naive, colour = method)) + geom_boxplot() + facet_wrap(~freq) +coord_flip()
+ggplot(data=results[lvl==1], aes(x= fc.item, y=rae.naive)) + geom_boxplot() + coord_flip()
+
+
+#res=f_results.load("E:/data/errors/reg_52_52_beer_3_.rds")
+#res.summary = f_results.summarise(res)
+
+
+
+x = readRDS("E:/data/errors/ets_52_12_3_beer_00-01-18200-53030.rds")
+max(x$o) ;x
+
+x[,max(o),by=fc.item]
+tail(x,100)
+
+
+
+
+
+
+
+
 
 ## load the consolidated results
 
-res = readRDS("E:/data/errors/all.rds")
+#res = readRDS("E:/data/errors/all.rds")
 
 res[,`:=`(freq = factor(freq, levels = c(12,52),labels=c("MONTH", "WEEK")),
           freq.cycle = factor(freq.cycle,levels = c(12,52),labels=c("MONTH", "WEEK")))]
